60bdd8ab29e2c6ab58608dd5319cd556ac721d32
Ibrahim Abdi <ibroabdi@outlook.com>
2026-07-18 18:28:00 +0100

    added to validateFile function
    the function now has a condition that checks file size

--- image_processor.cpp
+++ image_processor.cpp
@@ -7,13 +7,36 @@
 bool ImageProcessor::validateFile(const std::string& file) {
 	std::filesystem::path thisPath(file);
 
+	// check variable stores the boolean result of 
+	// whether a path to a file exists or not
+
 	bool check = std::filesystem::exists(thisPath);
 
 	if (check) {
-		std::cout << "The path exists! Yay" << std::endl;
-		return true;
+		std::cout << "The path exists! Wonderful." << std::endl;
 	}
 	else {
-		throw std::runtime_error("File doesn't exist");
-	}	
+		throw std::runtime_error("File doesn't exist.\n");
+	}
+
+	// checking that the actual contents of the file is not empty
+
+	if (!std::filesystem::is_empty(thisPath)) {
+		std::cout << "This file contains stuff, good." << std::endl;
+	}
+	else {
+		throw std::invalid_argument("File has no contents.\n");
+	}
+
+	// checking that the file is not corrupt, 
+	// comparing it against the expected byte size
+
+	const uintmax_t EXPECTED_SIZE = 540 * 1200 * sizeof(uint16_t);
+
+	if (std::filesystem::file_size(thisPath) != EXPECTED_SIZE) {
+		throw std::length_error("This file size was not expected.\n");
+	}
+
+	return true;
+
 }
