f0cdc4cca37227949c9823f7397923cc9bb1f189
Ibrahim Abdi <ibroabdi@outlook.com>
2026-07-18 19:07:54 +0100

    updated header file
    contains constants needed for comparisons and other checks, also initialised new function `loadRaw`

--- image_processor.h
+++ image_processor.h
@@ -1,8 +1,14 @@
 #pragma once
 #include <string>
-
+#include <vector>
 class ImageProcessor {
 public:
-	static bool validateFile(const std::string& file);
+	static constexpr size_t IMAGE_WIDTH = 540;
+	static constexpr size_t IMAGE_HEIGHT = 1200;
+	static constexpr size_t EXPECTED_ELEMENTS = IMAGE_HEIGHT * IMAGE_WIDTH;
+	static constexpr size_t EXPECTED_BYTES = EXPECTED_ELEMENTS * sizeof(uint16_t);
+
+	static bool validateFile(const std::string& filePath);
+	static std::vector<uint16_t> loadRaw(const std::string& filePath);
 };
 
