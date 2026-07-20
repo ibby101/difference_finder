6547cc93088a894bea0bd8c9111574c851af2cd6
Ibrahim Abdi <ibroabdi@outlook.com>
2026-07-20 20:48:12 +0100

    removed whitespace

--- image_processor.h
+++ image_processor.h
@@ -13,6 +13,5 @@ public:
 	static std::vector<uint16_t> loadRaw(const std::string& filePath);
 	static std::vector<uint16_t> calculateDiff(const std::vector<uint16_t>& imageA, const std::vector<uint16_t>& imageB);
 	static bool writeRaw(const std::vector<uint16_t> finalImage, const std::string& outputName);
-
 };
 
